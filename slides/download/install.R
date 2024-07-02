1db4f29d3d2f26f82414321d0c41f809acc900d9
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2024-07-02 21:13:09 +0200

    few adaptations for useR

--- slides/download/install.R
+++ slides/download/install.R
@@ -2,9 +2,6 @@ install.packages(c(
     "tidyverse",
     "testthat",
     "covr",
-    "shiny",
-    "shinytest2",
-    "profvis",
     "data.table",
     "usethis",
     "roxygen2",
@@ -20,12 +17,3 @@ install.packages(c(
     "pkgdown",
     "remotes"
 ))
-
-# Install using the remotes package
-remotes::install_github("rstudio/shinyuieditor")
-# For this package, please see https://rstudio.github.io/shinyuieditor/
-# for Installation instructions and
-# https://rstudio.github.io/shinyuieditor/#trouble-installing
-# if you have trouble with the installation.
-# It is not super essential for the workshop
-# so no worries if it does not install in the end.
